088fefb57054e68de73c1839404fe81f00341567
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-02-02 17:23:08 +0200

    Create main.py

---
+++ checker/main.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def get_sum(n, value):
+    result = np.sum([value, - 1])
+    for x in range(n):
+        result = np.sum([result, np.sum([x, 1])])
+    return result
+
+
+def get_product(n, value):
+    result = value
+    for x in range(n):
+        result = np.multiply(result, np.sum([x, 1]))
+    return result
