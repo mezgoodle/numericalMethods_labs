039d39ce0ba4c4046bd5f1fe6f3fbfa44a0dc6e8
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-02-02 17:33:44 +0200

    Update work with log

--- Lab1/main.py
+++ Lab1/main.py
@@ -3,6 +3,7 @@ from math import factorial, sqrt, e, log
 from checker import main
 
 # Calculating area start
+
 """Simple functions"""
 x = 10                                                                    # assigning value to a variable
 y = pow(x - 3, 2) + 1                                                     # assigning a calculation to a variable
@@ -18,7 +19,7 @@ print(factorial(5))                                                       # Fact
 print(abs(-10) == np.abs(-10))                                            # Module
 print(sqrt(4) == np.sqrt(4))                                              # Radical
 print(pow(125, (1 / 3)) == np.power(float(125), float(np.divide(1, 3))))
-print(e ** log(3, e) == np.power(np.e, float(np.log(3, np.e))))           # Logarithm
+print(e ** log(3, e) == np.power(np.e, float(np.log(3))))                 # Logarithm
 print(main.get_sum(10, 1))                                                # Summation
 print(main.get_product(10, 1))                                            # Product
 
