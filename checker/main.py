d9411d06edfd99121802477bc4f93d2edb277ba7
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-02-03 13:16:16 +0200

    Add comments for functions

--- checker/main.py
+++ checker/main.py
@@ -2,6 +2,7 @@ import numpy as np
 
 
 def get_sum(n, value):
+    """Get sum from value to n"""
     result = np.sum([value, - 1])
     for x in range(n):
         result = np.sum([result, np.sum([x, 1])])
@@ -9,6 +10,7 @@ def get_sum(n, value):
 
 
 def get_product(n, value):
+    """Get product from value to n"""
     result = value
     for x in range(n):
         result = np.multiply(result, np.sum([x, 1]))
@@ -16,6 +18,7 @@ def get_product(n, value):
 
 
 def increase_matrix(matrix, value):
+    """Multiply matrix with value"""
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             matrix[i][j] = np.multiply(matrix[i][j], value)
