2f6256fdfd2ddf4063e9e531487ac6f9cb92ff4f
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-03-10 16:52:08 +0200

    Update seidel.py

--- Lab3/seidel.py
+++ Lab3/seidel.py
@@ -4,38 +4,57 @@ import numpy as np
 # Defining our function as seidel which takes 3 arguments
 # as A matrix, Solution and B matrix
 
-def seidel(a, x, b):
-    x_ = x.copy()
-    n = len(a)
+def seidel(matrix_a: list, vector_b: list, vector_x: list) -> list:
+    """
+    Seidel algorithm part
+    :param matrix_a: start matrix
+    :param vector_b: start vector
+    :param vector_x: solution vector
+    :return:
+    """
+    x_ = vector_x.copy()
+    n = len(matrix_a)
     for j in range(0, n):
-        d = b[j]
+        d = vector_b[j]
 
         for i in range(0, n):
             if j != i:
-                d -= a[j][i] * x_[i]
-        x_[j] = d / a[j][j]
+                d -= matrix_a[j][i] * x_[i]
+        x_[j] = d / matrix_a[j][j]
     return x_
 
 
-x = [0, 0, 0, 0]
+def solve(matrix_a: list, vector_b: list, vector_x: list, eps=10 ** (-6)) -> list:
+    """
+    Main funtion
+    :param matrix_a: start matrix
+    :param vector_b: start vector
+    :param vector_x: solution vector
+    :param eps: epsilon for comparing
+    :return:
+    """
+    iterations = 0
+    while True:
+        errors = []
+        new_x = seidel(matrix_a, vector_b, vector_x)
+        for i in range(len(vector_x)):
+            errors.append(abs(new_x[i] - vector_x[i]))
+        vector_x = new_x
+        if max(errors) < eps:
+            break
+        else:
+            iterations += 1
+    print(f'Iterations: ', iterations)
+    return vector_x
+
+
 a = [[2.12, 0.42, 1.34, 0.88],
      [0.42, 3.95, 1.87, 0.43],
      [1.34, 1.87, 2.98, 0.46],
      [0.88, 0.43, 0.46, 4.44]]
 b = [11.172, 0.115, 0.009, 9.349]
-eps = 10 ** (-6)
-iterations = 0
+x = [0 for _ in range(len(a[0]))]
+x = solve(a.copy(), b.copy(), x.copy())
+print(x)
 
-while True:
-    errors = []
-    new_x = seidel(a, x, b)
-    for i in range(len(x)):
-        errors.append(abs(new_x[i] - x[i]))
-    x = new_x
-    if max(errors) < eps:
-        break
-    else:
-        iterations += 1
-    print(x)
-print(f'Iterations: ', iterations)
 print(np.linalg.solve(a, b))
