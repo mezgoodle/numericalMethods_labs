64b9028d86edb20173e26738f50c6a003b4640be
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-03-10 11:37:30 +0200

    Update jacobi.py
    Rename variables, add comments

--- Lab3/jacobi.py
+++ Lab3/jacobi.py
@@ -1,37 +1,48 @@
 import numpy as np
 
 
-def jacobi(a: list, b: list, eps=10 ** (-6)) -> list:
-    x = [0 for _ in range(len(a[0]))]
+def solve_jacobi(matrix_a: list, vector_b: list, epsilon=10 ** (-6)) -> list:
+    """
+    Solve by Jacobi method(simple iteration)
+    :param matrix_a: start matrix
+    :param vector_b: start vector
+    :param epsilon: number for comparing
+    :return: solution vector
+    """
+    solution_vector = [0 for _ in range(len(matrix_a[0]))]
 
-    c = []
-    d = []
-    for i in range(len(a)):
-        element_d = b[i] / a[i][i]
-        d.append(element_d)
+    matrix_c = []
+    vector_d = []
+    # Create matrix C and vector D
+    for i in range(len(matrix_a)):
+        element_d = vector_b[i] / matrix_a[i][i]
+        vector_d.append(element_d)
 
         element_c = []
-        for j in range(len(a[0])):
+        for j in range(len(matrix_a[0])):
             if i == j:
                 element_c.append(0)
             else:
-                element_c.append((-1) * a[i][j] / a[i][i])
-        c.append(element_c)
+                element_c.append((-1) * matrix_a[i][j] / matrix_a[i][i])
+        matrix_c.append(element_c)
 
     iterations = 0
+    # Start the main algorithm
     while True:
-        errors = []
-        rightP = np.dot(c, x)
-        for i in range(len(x)):
-            x_next = rightP[i] + d[i]
-            errors.append(abs(x_next - x[i]))
-            x[i] = x_next
-        if max(errors) < eps:
+        divs = []
+        left_part = np.dot(matrix_c, solution_vector)
+        for i in range(len(solution_vector)):
+            x_next = left_part[i] + vector_d[i]
+            divs.append(abs(x_next - solution_vector[i]))
+            solution_vector[i] = x_next
+        # Check if we need to stop
+        if max(divs) < epsilon:
+            # It's time to stop!
             break
         else:
             iterations += 1
     print(f'Iterations: {iterations}')
-    return x
+    return solution_vector
 
 
 a = [[2.12, 0.42, 1.34, 0.88],
@@ -39,7 +50,8 @@ a = [[2.12, 0.42, 1.34, 0.88],
      [1.34, 1.87, 2.98, 0.46],
      [0.88, 0.43, 0.46, 4.44]]
 b = [11.172, 0.115, 0.009, 9.349]
-
-sol = jacobi(a, b)
-print(np.matrix(sol))
-print(np.linalg.solve(a, b))
+print(f'Matrix A:', np.matrix(a))
+print(f'Vector b:', b)
+sol = solve_jacobi(a.copy(), b.copy())
+print(f'Our solution: {np.matrix(sol)}')
+print(f'NumPy solution: {np.linalg.solve(a, b)}')
