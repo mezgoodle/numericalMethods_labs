57bde0238f73d93f93c7d025194b860698bb5dbf
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-03-14 19:19:39 +0200

    Update jacobi.py
    Add round

--- Lab3/jacobi.py
+++ Lab3/jacobi.py
@@ -48,7 +48,7 @@ def solve_jacobi(matrix_a: list, vector_b: list, epsilon=10 ** (-6)) -> list:
             if tmp < 3:
                 print(f'Temporary result: {solution_vector}')
                 tmp += 1
-            print(f'Residual vector: {np.matrix(np.subtract(vector_b, np.dot(matrix_a, solution_vector)), float)}')
+            print(f'Residual vector: {np.matrix(np.subtract(vector_b, np.dot(matrix_a, solution_vector)), float).round(6)}')
             iterations += 1
     print(f'Iterations: {iterations}')
     return solution_vector
@@ -59,12 +59,12 @@ a = [[4.4944, 0.1764, 1.7956, 0.7744],
      [1.7956, 3.4969, 8.8804, 0.2116],
      [0.7744, 0.1849, 0.2116, 19.7136]]
 b = [31.97212, 9.18339, 19.51289, 51.39451]
-print(f'Matrix A:', np.matrix(a))
+print(f'Matrix A:', np.matrix(a).round(6))
 print(f'Vector b:', b)
 sol = solve_jacobi(a.copy(), b.copy())
 sol_np = np.linalg.solve(a, b)
-print(f'Our solution: {np.matrix(sol)}')
-print(f'Residual vector: {np.matrix(np.subtract(b, np.dot(a, sol)))}')
+print(f'Our solution: {np.matrix(sol).round(6)}')
+print(f'Residual vector: {np.matrix(np.subtract(b, np.dot(a, sol))).round(6)}')
 print(f'NumPy solution: {sol_np}')
-print(f'Residual vector for NumPy: {np.matrix(np.subtract(b, np.dot(a, sol_np)))}')
+print(f'Residual vector for NumPy: {np.matrix(np.subtract(b, np.dot(a, sol_np))).round(6)}')
 print('Fault:', round(get_fault(sol, sol_np), 6))
