bdb89eec1737139904bbfbd8a500cda2245aed2b
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-03-24 16:51:05 +0200

    Update main.py
    Some updates

--- Lab4/main.py
+++ Lab4/main.py
@@ -9,25 +9,47 @@ a = [
     [1.24, 0.16, 2.1, 6.1]
 ]
 
+print('Matrix A:')
 print(np.matrix(a))
 
 b = np.zeros((4, 4))
 np.fill_diagonal(b, 1)
-b_minus1 = b.copy()
-print(b)
-print(b_minus1)
+b1_minus = b.copy()
+b2 = b.copy()
+b2_minus = b.copy()
 
 b[2][0] = a[3][0] / a[3][2] * (-1)
 b[2][1] = a[3][1] / a[3][2] * (-1)
 b[2][2] = 1 / a[3][2]
 b[2][3] = a[3][3] / a[3][2] * (-1)
+print('Matrix B1:')
 print(b)
 
-b_minus1[2][0] = a[3][0]
-b_minus1[2][1] = a[3][1]
-b_minus1[2][2] = a[3][2]
-b_minus1[2][3] = a[3][3]
-print(b_minus1)
+b1_minus[2][0] = a[3][0]
+b1_minus[2][1] = a[3][1]
+b1_minus[2][2] = a[3][2]
+b1_minus[2][3] = a[3][3]
+print('Matrix B1-1:')
+print(b1_minus)
 
-temp = np.dot(b_minus1, np.dot(a, b))
+temp = np.dot(b1_minus, np.dot(a, b))
+print('Matrix temp:')
+print(temp)
+
+b2[1][0] = temp[2][0] / temp[2][1] * (-1)
+b2[1][1] = 1 / temp[2][1]
+b2[1][2] = temp[2][2] / temp[2][1] * (-1)
+b2[1][3] = temp[2][3] / temp[2][1] * (-1)
+print('Matrix B2:')
+print(b2)
+
+b2_minus[1][0] = temp[2][0]
+b2_minus[1][1] = temp[2][1]
+b2_minus[1][2] = temp[2][2]
+b2_minus[1][3] = temp[2][3]
+print('Matrix B2-1:')
+print(b2_minus)
+
+temp = np.dot(b2_minus, np.dot(temp, b2))
+print('Matrix temp:')
 print(temp)
