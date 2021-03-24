a4fa78e55cb0f24da8cf40b273f8ee054c32ec45
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-03-24 16:43:25 +0200

    Update main.py
    Fix indexes

--- Lab4/main.py
+++ Lab4/main.py
@@ -8,21 +8,26 @@ a = [
     [0.97, 1.3, 5.44, 2.1],
     [1.24, 0.16, 2.1, 6.1]
 ]
+
 print(np.matrix(a))
+
 b = np.zeros((4, 4))
 np.fill_diagonal(b, 1)
-b1 = b.copy()
+b_minus1 = b.copy()
 print(b)
-print(b1)
-b[2][0] = a[0][3] / a[2][3] * (-1)
-b[2][1] = a[1][3] / a[2][3] * (-1)
-b[2][2] = 1 / a[2][3]
-b[2][3] = a[3][3] / a[2][3] * (-1)
+print(b_minus1)
+
+b[2][0] = a[3][0] / a[3][2] * (-1)
+b[2][1] = a[3][1] / a[3][2] * (-1)
+b[2][2] = 1 / a[3][2]
+b[2][3] = a[3][3] / a[3][2] * (-1)
 print(b)
-b1[2][0] = a[0][3]
-b1[2][1] = a[1][3]
-b1[2][2] = a[2][3]
-b1[2][3] = a[3][3]
-print(b1)
-temp = np.dot(b1,np.dot(a, b))
+
+b_minus1[2][0] = a[3][0]
+b_minus1[2][1] = a[3][1]
+b_minus1[2][2] = a[3][2]
+b_minus1[2][3] = a[3][3]
+print(b_minus1)
+
+temp = np.dot(b_minus1, np.dot(a, b))
 print(temp)
