bf042094942d48175e8ecf21d937b7810913e2ea
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-05-22 22:02:42 +0300

    Update main.py

--- Lab8/main.py
+++ Lab8/main.py
@@ -1,6 +1,6 @@
 from tabulate import tabulate
 import pandas as pd
-import numpy as np
+from string import Template
 from math import e
 
 n = 5
@@ -9,6 +9,7 @@ interval = [0, 4]
 h = 0.1
 x0 = y0 = 0
 epsilon = 10 ** (-1)
+template = Template('#' * 10 + ' $string ' + '#' * 10)
 
 
 def dfunction(x: float, y: float) -> float:
@@ -22,6 +23,7 @@ def dfunction(x: float, y: float) -> float:
 
 
 def runge_kutte_method(interval, h, epsilon, x0, y0):
+    rg_res = []
     table = []
     table.append([x0, y0, 0, 0])
     xi = x0
@@ -38,7 +40,37 @@ def runge_kutte_method(interval, h, epsilon, x0, y0):
         if fault > epsilon:
             h /= 2
         table.append([xi, yi, delta_y, fault])
+        rg_res.append([xi, yi])
     print_table(table, ('x', 'y', 'Delta y', 'Fault'))
+    return rg_res
+
+
+def adams_method(interval, h, epsilon, rg_res):
+    table = []
+    table.append([rg_res[0][0], rg_res[0][1], 0])
+    table.append([rg_res[1][0], rg_res[1][1], 0])
+    table.append([rg_res[2][0], rg_res[2][1], 0])
+    i = 3
+    step = h
+    while i < ((interval[1] - interval[0]) / h):
+        k1 = dfunction(rg_res[i][0], rg_res[i][1])
+        k2 = dfunction(rg_res[i - 1][0], rg_res[i - 1][1])
+        k3 = dfunction(rg_res[i - 2][0], rg_res[i - 2][1])
+        k4 = dfunction(rg_res[i - 3][0], rg_res[i - 3][1])
+        extra_y = rg_res[i][1] + h / 24 * (55 * k1 - 59 * k2 + 37 * k3 - 9 * k4)
+        next_x = rg_res[i][0] + step
+        intra_y = rg_res[i][1] + h / 24 * (9 * dfunction(next_x, extra_y) + 19 * k1 - 5 * k2 + k3)
+        fault = abs(intra_y - extra_y)
+        if fault > epsilon:
+            step / 2
+        if extra_y == intra_y:
+            table.append([rg_res[i][0], extra_y, fault])
+            rg_res.append([next_x, extra_y])
+        else:
+            table.append([rg_res[i][0], intra_y, fault])
+            rg_res.append([next_x, intra_y])
+        i += 1
+    print_table(table, ('x', 'y', 'Fault'))
 
 
 def print_table(table: list, headers: tuple):
@@ -46,4 +78,7 @@ def print_table(table: list, headers: tuple):
     print(tabulate(df, headers=headers, tablefmt='github'))
 
 
-runge_kutte_method(interval, h, epsilon, x0, y0)
+print(template.substitute(string='Runge-kutta method'))
+rg_res = runge_kutte_method(interval, h, epsilon, x0, y0)
+print(template.substitute(string='Adams method'))
+adams_method(interval, h, epsilon, rg_res[:4])
