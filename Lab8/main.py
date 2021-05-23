964db2d9038a9056e1042f6a295cf92e9d6bf103
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-05-23 21:53:51 +0300

    Update main.py

--- Lab8/main.py
+++ Lab8/main.py
@@ -44,7 +44,6 @@ def show_plot(x_values: list, y_values: list) -> None:
 
 
 def runge_kutte_method(interval, h, epsilon, x0, y0):
-    rg_res = [[x0, y0]]
     table = []
     table.append([x0, y0, 0])
     xi = x0
@@ -61,16 +60,10 @@ def runge_kutte_method(interval, h, epsilon, x0, y0):
         if fault > epsilon:
             h /= 2
         table.append([xi, yi])
-        rg_res.append([xi, yi])
-    return rg_res, table
+    return table
 
 
 def adams_method(interval, h, epsilon, rg_res):
-    table = []
-    table.append([rg_res[0][0], rg_res[0][1]])
-    table.append([rg_res[1][0], rg_res[1][1]])
-    table.append([rg_res[2][0], rg_res[2][1]])
-    table.append([rg_res[3][0], rg_res[3][1]])
     i = 3
     step = h
     while i < ((interval[1] - interval[0]) / h):
@@ -85,13 +78,11 @@ def adams_method(interval, h, epsilon, rg_res):
         if fault > epsilon:
             step / 2
         if extra_y == intra_y:
-            table.append([next_x, extra_y, fault])
             rg_res.append([next_x, extra_y])
         else:
-            table.append([next_x, intra_y])
             rg_res.append([next_x, intra_y])
         i += 1
-    return rg_res, table
+    return rg_res
 
 
 def search_error_for_rg(rg_res, h):
@@ -122,16 +113,19 @@ def print_table(table: list, headers: tuple):
 
 
 print(template.substitute(string='Runge-kutta method'))
-rg_res, table = runge_kutte_method(interval, h, epsilon, x0, y0)
-rg_res_less, table_ = runge_kutte_method(interval, h / 2, epsilon, x0, y0)
+rg_res= runge_kutte_method(interval, h, epsilon, x0, y0)
+rg_res_less = runge_kutte_method(interval, h / 2, epsilon, x0, y0)
 errors = search_error_for_ad(rg_res, rg_res_less)
 for i in range(1, len(errors)):
-    table[i].append(abs(errors[i]))
-print_table(table, ('x', 'y', 'Error'))
+    rg_res[i].append(abs(errors[i]))
+print_table(rg_res, ('x', 'y', 'Error'))
 print(template.substitute(string='Adams method'))
-ad_res, table_finish = adams_method(interval, h, epsilon, rg_res[:4])
-ad_res_less, table = adams_method(interval, h / 2, epsilon, rg_res_less[:4])
+ad_res = adams_method(interval, h, epsilon, rg_res[:4])
+ad_res_less = adams_method(interval, h / 2, epsilon, rg_res_less[:4])
 errors = search_error_for_ad(ad_res, ad_res_less)
 for i in range(len(errors)):
-    table_finish[i].append(abs(errors[i]))
-print_table(table_finish, ('x', 'y', 'Error'))
+    if i < 4:
+        ad_res[i][2] = abs(errors[i])
+    else:
+        ad_res[i].append(abs(errors[i]))
+print_table(ad_res, ('x', 'y', 'Error'))
